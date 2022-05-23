0da8d5b3fc5b86a50315e0d912cde8db44942537
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-05-23 05:09:43 -0300

    Preparando revisão de código

--- src/services/e2eService.ts
+++
@@ -1,9 +0,0 @@
-import { e2eRepository } from "../repositories/e2eRepository";
-
-async function resetDatabase() {
-  return await e2eRepository.resetDatabase();
-}
-
-export const e2eService = {
-  resetDatabase,
-}
